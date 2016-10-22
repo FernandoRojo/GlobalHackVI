cb55ebe8b9595a5eff0bc650e391a2814ffa0c6e
GHVI <GHVI@ip-172-31-28-166.us-west-2.compute.internal>
2016-10-22 11:34:42 -0500

    Added Auth Directories, Heat Maps, and formed outline for Shelter Database

--- GlobalHack/Shelters/views.py
+++ GlobalHack/Shelters/views.py
@@ -3,17 +3,19 @@ from django.conf.urls import url
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from .forms import ShelterForm
+from .models import MakeShelter
 # Create your views here.
 import feedparser
 
 def index(request):
     return render(request, 'rssfeed/index.html');
-
+def Map(request):
+    return render(request, 'Shelters/heatmap.html');
 
 def NewShelter(request):
     if request.method == 'POST':
-        ShelterForm(request.POST)
-        if form.is_valid():
+        form = ShelterForm(request.POST)
+        if form.is_valid() and MakeShelter(form.data):
             return HttpResponse("Thanks");
     else:
         form = ShelterForm();
